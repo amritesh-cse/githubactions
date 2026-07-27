a11c6007a4cc56bb5f043eb263e94f49cdcedeab
amritesh-cse <amritesh.cse@gmail.com>
2026-07-28 03:25:16 +0530

    code

---
+++ src/main/java/com/githubactions/GithubactionsApplication.java
@@ -0,0 +1,30 @@
+package com.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubactionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello World - Github Actions";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionsApplication.class, args);
+    }
+/*
+    echo "# githubactions" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/amritesh-cse/githubactions.git
+    git push -u origin main
+
+ */
+}
